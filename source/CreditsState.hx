02ccc18ec700cfa0b3f8a59dc8193a2b5e130d37
SM64SR <67247243+SM64SR@users.noreply.github.com>
2022-04-25 09:51:00 -0400

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -331,5 +331,33 @@ class CreditsState extends MusicBeatState
 
 	private function unselectableCheck(num:Int):Bool {
 		return creditsStuff[num].length <= 1;
+		        #if android
+	addVirtualPad(FULL, A_B);
+        #end
+
+	//if you want it to have a camera
+        #if android
+	addPadCamera();
+        #end
+
+	//in states, those needs to be added before super.create();
+	//in substates, in fuction new at the last line add those
+
+	//on Playstate.hx after all
+	//obj.camera = ...
+	//add
+        #if android
+	addAndroidControls();
+        #end
+
+	//to make the controls visible the code is
+	#if android
+	androidc.visible = true;
+	#end
+
+	//to make the controls invisible the code is
+	#if android
+	androidc.visible = false;
+	#end
 	}
 }
