227eff923bfb4fe23fbc7458238e4f3327bf75d7
SM64SR <67247243+SM64SR@users.noreply.github.com>
2022-04-25 09:50:10 -0400

    Update AchievementsMenuState.hx

--- source/AchievementsMenuState.hx
+++ source/AchievementsMenuState.hx
@@ -113,6 +113,34 @@ class AchievementsMenuState extends MusicBeatState
 		}
 		descText.text = Achievements.achievementsStuff[achievementIndex[curSelected]][1];
 		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+		        #if android
+	addVirtualPad(FULL, A_B);
+        #end
+
+	//if you want it to have a camera
+        #if android
+	addPadCamera();
+        #end
+
+	//in states, those needs to be added before super.create();
+	//in substates, in fuction new at the last line add those
+
+	//on Playstate.hx after all
+	//obj.camera = ...
+	//add
+        #if android
+	addAndroidControls();
+        #end
+
+	//to make the controls visible the code is
+	#if android
+	androidc.visible = true;
+	#end
+
+	//to make the controls invisible the code is
+	#if android
+	androidc.visible = false;
+	#end
 	}
 	#end
 }
